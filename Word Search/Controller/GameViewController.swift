8d0294837a684e3beaba32d7e1f979f3dc564061
Kayo <hnm@i.softbank.jp>
2019-05-10 18:25:47 -0400

    Complete display grid view (UICollectionView)

--- Word Search/Controller/GameViewController.swift
+++ Word Search/Controller/GameViewController.swift
@@ -10,14 +10,55 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
     var level: Level!
+    var screenSize = CGRect()
+    
+    let colors:[UIColor] = [UIColor.magenta, UIColor.green, UIColor.yellow]
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "Level " + level.level
+        collectionView.backgroundColor = UIColor.blue
+        
+        screenSize = UIScreen.main.bounds
+        collectionView.contentSize = CGSize(width: screenSize.size.width, height: screenSize.size.height)
+        
     }
 
 
+}
+
+extension GameViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return level.grid * level.grid
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "gameCell", for: indexPath)
+        
+        cell.backgroundColor = UIColor.red
+        
+        for view in cell.subviews{
+            if let label = view as? UILabel{
+                let index = Int(arc4random_uniform(3))
+                label.backgroundColor = colors[index]
+            }
+        
+        }
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let width:CGFloat = collectionView.contentSize.width / CGFloat(level.grid)
+        
+        return CGSize(width: width, height: width)
+    }
+    
+    
 }
 
---
+++ Word Search/View/CustomCollectionViewCell.swift
@@ -0,0 +1,46 @@
+//
+//  CustomCollectionViewCell.swift
+//  Word Search
+//
+//  Created by 中尾 佳代 on 2019/05/10.
+//  Copyright © 2019 Kayo Nakao. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+    
+        required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupViews()
+    }
+    
+    let letterLabel:UILabel = {
+        let label = UILabel()
+        label.text = "A"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        return label
+    }()
+    
+    func setupViews(){
+        self.addSubview(letterLabel)
+        
+        
+        let heightConstraint = NSLayoutConstraint(item: letterLabel, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.frame.size.height)
+        
+        let widthConstraint = NSLayoutConstraint(item: letterLabel, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: self.frame.size.width)
+
+        NSLayoutConstraint.activate([
+            letterLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            letterLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            heightConstraint, widthConstraint
+            ])
+        
+        letterLabel.font = letterLabel.font.withSize(letterLabel.frame.size.width)
+
+    }
+    
+    
+    
+}
