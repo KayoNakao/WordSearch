2c1b766c34bb7b142d70ae928a4bdb8c2d0dd59f
Kayo <hnm@i.softbank.jp>
2019-05-10 09:33:12 -0400

    Initial Commit

---
+++ Word Search/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Word Search
+//
+//  Created by 中尾 佳代 on 2019/05/10.
+//  Copyright © 2019 Kayo Nakao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
